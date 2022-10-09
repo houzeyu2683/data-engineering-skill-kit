b8e14e132702cddd12a20dff8611f5777b39acaf
Greg <houzeyu2683@gmail.com>
2022-10-09 19:03:58 +0800

    'try'

---
+++ data.py
@@ -0,0 +1,45 @@
+
+import pandas
+import os
+
+class table:
+
+    def __init__(self, folder):
+
+        self.folder = folder
+        return
+
+    def scan(self, style='csv'):
+
+        path = []
+        name = []
+        loop = os.listdir(self.folder)
+        for i in loop:
+
+            if(style=='csv'):
+
+                condition = style in i.split('.')[-1]
+                if(condition):
+
+                    p = os.path.join(self.folder, i)
+                    n = i.replace(".{}".format(style), "")
+                    path += [p]
+                    name += [n]
+                    pass
+
+                pass
+
+            continue
+        
+        self.path = path
+        self.name = name
+        return
+
+    def get(self, name):
+
+        i = self.name.index(name)
+        p = self.path[i]
+        table = pandas.read_csv(p)
+        return(table)
+
+    pass
---
+++ setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name='desk',
+    version='0.0.0',
+    author='Greg Hou',
+    author_email='houzeyu2683@gmail.com',
+    description='Data Engineering Skill Kit',
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url='https://github.com/houzeyu2683/desk',
+    # project_urls = {
+    #     "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+    # },
+    # license='MIT',
+    packages=['desk'],
+    install_requires=['pandas']
+)
