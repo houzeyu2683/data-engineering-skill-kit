4ca7eed2cd502a21f96fe6b4d3e50d152dd0c0dd
Greg <houzeyu2683@gmail.com>
2022-10-09 19:18:07 +0800

    'try'

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
 
 setuptools.setup(
     name='desk',
@@ -10,13 +10,13 @@ setuptools.setup(
     author='Greg Hou',
     author_email='houzeyu2683@gmail.com',
     description='Data Engineering Skill Kit',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
     url='https://github.com/houzeyu2683/desk',
-    project_urls = {
-        "Bug Tracker": "https://github.com/houzeyu2683/desk/issues"
-    },
-    license='MIT',
+    # project_urls = {
+    #     "Bug Tracker": "https://github.com/houzeyu2683/desk/issues"
+    # },
+    # license='MIT',
     packages=['desk'],
     install_requires=['pandas']
 )
