5e27f758bc0a1eee63f2b0daced7891ea000e7a4
Greg <houzeyu2683@gmail.com>
2022-11-10 16:48:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 import setuptools
 
 setuptools.setup(
-    name='de',
+    name='data-engineering-skill-kit',
     version='0.0.0',
     author='Greg Hou',
     author_email='houzeyu2683@gmail.com',
-    description='Data Engineering Skill Kit',
+    description='Data/Feature engineering function collection, more high level application.',
     url='https://github.com/houzeyu2683/de',
-    packages=['sk'],
-    install_requires=['pandas']
+    packages=['desk']
+#    install_requires=['pandas']
 )
